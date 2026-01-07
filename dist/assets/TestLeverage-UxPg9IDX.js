66b2f9005c414aded186e21016c807b3f15197bb
ocean <anxia0818@gmail.com>
2026-01-07 10:32:29 +0800

    update

--- dist/assets/TestLeverage-UxPg9IDX.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as r}from"./vendor-5TsuyGYQ.js";import{u as s}from"./useGetSuiLendInfo-CGFIfRGO.js";import"./index-Bdw5mqi3.js";import"./cross-DIh4_PqK.js";function u(){const{getLendingMarketData:e}=s();return t.useEffect(()=>{e()},[]),r.jsx("div",{children:"TestLeverage"})}export{u as default};
