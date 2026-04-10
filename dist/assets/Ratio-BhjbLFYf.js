be206387ddbf07c51fbcc8a883fe8bad5e508f07
ocean <anxia0818@gmail.com>
2026-04-10 16:15:07 +0800

    update

--- dist/assets/Ratio-BhjbLFYf.js
+++
@@ -1,1 +0,0 @@
-import{j as i,T as e}from"./vendor-Cq18sjpg.js";import{K as s}from"./index-CKDg_YJr.js";import"./cross-BcmOT8Fu.js";function x({value:t}){const r=s(t).gte(0),o=Number(t).toFixed(2);return i.jsx(e,{fontSize:"12px",fontWeight:"500",color:r?"primary_green":"primary_red",children:r?`+${o}%`:`${o}%`})}export{x as R};
--- dist/assets/TestLeverage-CevjQeC5.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as r}from"./vendor-Cq18sjpg.js";import{u as s}from"./useGetSuiLendInfo-rSPHVSrA.js";import"./index-CKDg_YJr.js";import"./cross-BcmOT8Fu.js";function u(){const{getLendingMarketData:e}=s();return t.useEffect(()=>{e()},[]),r.jsx("div",{children:"TestLeverage"})}export{u as default};
