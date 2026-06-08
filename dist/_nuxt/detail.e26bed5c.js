45a9892747f11d0deea929da3618522165d0d588
ocean <anxia0818@gmail.com>
2026-06-08 14:06:24 +0800

    fix: update

---
+++ dist/_nuxt/detail.e26bed5c.js
@@ -0,0 +1,1 @@
+import{D as e}from"./entry.493f7c50.js";const n=e((t,i)=>{const o=document.querySelector(".body-container-bg");o&&(["pool-auto","pool-list","pool-position"].includes(t==null?void 0:t.name)&&(o==null?void 0:o.clientWidth)<750?o.scrollTo({top:580}):o.scrollTo({top:0}))});export{n as default};
--- dist/_nuxt/index.78fde84f.js
+++
@@ -1,1 +0,0 @@
-import t from"./vesting.0d73d362.js";import"./index.5401f333.js";import"./entry.1503cc1d.js";const r=t;export{r as default};
