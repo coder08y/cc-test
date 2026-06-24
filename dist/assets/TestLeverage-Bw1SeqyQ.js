2ddc011dac705c854970051540e320cd3fb42597
ocean <anxia0818@gmail.com>
2026-06-24 19:23:53 +0800

    chore: publish cc-test dist

--- dist/assets/TestLeverage-Bw1SeqyQ.js
+++
@@ -1,1 +0,0 @@
-import{r as t,j as r}from"./vendor-Cc2WthaN.js";import{u as s}from"./useGetSuiLendInfo-D9BEeka_.js";import"./index-DEUtHgBW.js";import"./cross-Bgu0h-7y.js";function u(){const{getLendingMarketData:e}=s();return t.useEffect(()=>{e()},[]),r.jsx("div",{children:"TestLeverage"})}export{u as default};
---
+++ dist/assets/_defineProperty-SzUgX-fZ.js
@@ -0,0 +1,1 @@
+import{b_ as r}from"./index-DIFNoch6.js";var o=function(){try{var e=r(Object,"defineProperty");return e({},"",{}),e}catch{}}();export{o as d};
