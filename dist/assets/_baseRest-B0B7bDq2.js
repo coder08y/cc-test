81ea28752e5ef9202371e9e736f61a47e2edd44c
ocean <anxia0818@gmail.com>
2026-07-07 18:14:31 +0800

    chore: publish cc-test dist

---
+++ dist/assets/_baseRest-B0B7bDq2.js
@@ -0,0 +1,1 @@
+import{cj as r}from"./index-BIGsOm9A.js";import{s,o as e}from"./last-CJsrO_cH.js";function m(t,o){return s(e(t,o,r),t+"")}export{m as b};
--- dist/assets/_baseRest-CCPnhs7H.js
+++
@@ -1,1 +0,0 @@
-import{cj as r}from"./index-BCzOaItG.js";import{s,o as e}from"./last-_M-T8C8h.js";function m(t,o){return s(e(t,o,r),t+"")}export{m as b};
